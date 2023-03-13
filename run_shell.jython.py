f58ec07576ff18155b73426affdcd33d1921b490
[Silverainox] <serainox@gmail.com>
2023-03-13 19:53:44 +0100

    Update run_shell.jython.py

--- run_shell.jython.py
+++ run_shell.jython.py
@@ -1,2 +1,5 @@
+# Execute shell commands (Bash / Powershell)
+# Use mplayer cli to play media from .minecraft as example
+
 from java.lang import Runtime
 Runtime.getRuntime().exec("mplayer xp.gif") 
